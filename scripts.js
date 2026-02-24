c84d0df14b2a7d5fe17f6ca92ffe93464faaada5
Nick Huynh <114318124+Protokol42@users.noreply.github.com>
2026-02-23 21:08:54 -0800

    Add score variables

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ console.log(getComputerChoice());
 
 console.log("The player chooses: " + getHumanChoice());
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const randomized = Math.random();
   if (randomized <= 0.33) return "rock";
