ad05840361bd4515929ca6b29db6a6a5214258b5
Nick Huynh <114318124+Protokol42@users.noreply.github.com>
2026-02-23 19:50:54 -0800

    Add Computer choice and Player choice functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 console.log("Hello");
+
+console.log(getComputerChoice());
+
+console.log("The player chooses: " + getHumanChoice());
+
+function getComputerChoice() {
+  const randomized = Math.random();
+  if (randomized <= 0.33) return "rock";
+  return randomized > 0.66 ? "scissors" : "paper";
+}
+
+function getHumanChoice() {
+  return prompt("Choose rock, paper or scissors!");
+}
