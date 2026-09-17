d3b053a7d90f8b6f901202b78f2eb0bf6fc76ed6
Nick Huynh <114318124+Protokol42@users.noreply.github.com>
2026-09-17 10:26:02 -0700

    Refactor the game to display score and winner

--- scripts.js
+++ scripts.js
@@ -1,24 +1,29 @@
-//playGame();
+const menu = document.querySelector(".menu");
+const gameButtons = menu.querySelectorAll("button");
+const playerPara = document.querySelector("#player");
+const computerPara = document.querySelector("#computer");
+const announcerPara = document.querySelector("#announcer");
+const playerScorePara = document.querySelector("#playerScore");
+const computerScorePara = document.querySelector("#computerScore");
+const roundNumberPara = document.querySelector("#roundNumber");
+
+const gameState = {
+  roundNumber: 0,
+  playerScore: 0,
+  computerScore: 0,
+};
+
 function getComputerChoice() {
   const randomized = Math.random();
   if (randomized <= 0.33) return "rock";
   return randomized > 0.66 ? "scissors" : "paper";
 }
-
-function getHumanChoice() {
-  return prompt("Choose rock, paper or scissors!").trim();
-}
-
-let menu = document.querySelector(".menu");
-let playerPara = document.querySelector("#player");
-let computerPara = document.querySelector("#computer");
-let announcerPara = document.querySelector("#announcer");
-
 menu.addEventListener("click", (event) => {
   let target = event.target;
   if (target === menu) return;
   let playerSelection = target.id;
-  playRound(playerSelection, getComputerChoice());
+  keepScore(playRound(playerSelection, getComputerChoice()));
+  roundStatusDisplayUpdate();
 });
 
 function playRound(humanChoice, computerChoice) {
@@ -69,31 +74,31 @@ function handleRoundAnnouncement(message, humanChoice, computerChoice) {
       break;
   }
 }
+function toggleGameState() {
+  gameButtons.forEach((button) => {
+    button.disabled = !button.disabled;
+  });
+}
+function keepScore(roundResult) {
+  gameState.roundNumber++;
+  if (roundResult === "computerWin") gameState.computerScore++;
+  else if (roundResult === "playerWin") gameState.playerScore++;
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  for (let step = 0; step < 5; step++) {
-    let result = playRound(getHumanChoice(), getComputerChoice());
-    if (result === "computerWin") computerScore++;
-    else if (result === "playerWin") humanScore++;
-    console.log(`You've played ${step + 1} round(s)!`);
-    console.log(
-      `Player: ${humanScore} points. Computer: ${computerScore} points.`,
-    );
-    console.log("---------------------------------------------");
+  if (gameState.computerScore === 5) {
+    announcerPara.textContent =
+      "The computer wins! It is the first to 5 points.";
+    toggleGameState();
+  } else if (gameState.playerScore === 5) {
+    announcerPara.textContent =
+      "The player wins! They are the first to 5 points.";
+    toggleGameState();
   }
+}
 
-  console.log("Game Over!");
-  console.log(
-    `Player: ${humanScore} points. Computer: ${computerScore} points.`,
-  );
-  if (humanScore === computerScore) console.log("It's a tie!");
-  else
-    humanScore > computerScore
-      ? console.log("The Player wins!")
-      : console.log("The Computer wins!");
+function roundStatusDisplayUpdate() {
+  roundNumberPara.textContent = `The current round number: ${gameState.roundNumber}`;
+  computerScorePara.textContent = `The Computer has ${gameState.computerScore} points`;
+  playerScorePara.textContent = `The player has ${gameState.playerScore} points`;
 }
 
 // Does not take empty strings.
